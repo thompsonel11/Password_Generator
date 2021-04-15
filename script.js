069c7ffa07e588792f4ad4e0842b5a42f65641bb
Liz Thompson <thompsonel2011@gmail.com>
2021-04-15 15:26:09 -0400

    add comments and correct for-loop

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Assignment Code
+// Initialize Variables 
 var generateBtn = document.querySelector("#generate")
 var includeSpecialChar = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '/', '?', ']', ' ', '{', '}', '|', '`', '~', '=', '+', ';', ':']
 var includeUpperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
@@ -8,20 +8,20 @@ var confirmLength = 0;
 var userSelections = [];
 var finalPassword = [];
 
-// User Selection Confirmation - begin password generation
+// Utilize a 'while-Loop' so that user must enter valid value to proceed
 function generatePassword () {
 while (confirmLength < 8 || confirmLength > 128 || confirmLength === ''){
 
-// Evaluate Password Length
+// Utilize 'prompt' to evaluate password length and command user to enter valid value if neccessary
   confirmLength = prompt('How many characters would you like your password to be? Please select a number between 8-128. ');
     if (confirmLength < 8 || confirmLength > 128){
         confirmLength = prompt ('Please enter a value between 8 and 128.')
       }
-      else if (Number.isNaN(parseInt(confirmLength))|| confirmLength === '') { 
+      else if (Number.isNaN(parseInt(confirmLength)) || confirmLength === '') { 
         confirmLength = prompt('Please enter a valid number between 8 and 128')
       }
-    }
-         
+ }
+// Utilize 'confirm' and 'push' to begin generating password based on character selection        
       // Special Characters
       var confirmSpecial = confirm('Do you want to include special characters?');
         if (confirmSpecial === true){
@@ -53,22 +53,27 @@ while (confirmLength < 8 || confirmLength > 128 || confirmLength === ''){
         console.log(userSelections, 'userInputSelection');
 
 
-      // If user didn't select special characters, upper/lower case or numbers 
-    if (confirmSpecial === false && confirmUppercase === false && confirmLowercase === false && confirmNumeric === false) {
-        console.log('No character type was selected');
-        alert("Please select at least one character type");
+// Utilizing 'alert' to alert the user to include at least one character choice: special characters, upper/lower case or numbers 
+        if (confirmSpecial === false && confirmUppercase === false && confirmLowercase === false && confirmNumeric === false) {
+           console.log('No character type was selected');
+           alert("Please select at least one character type");
         return;
   }
-        
+  
+    // For loop that will create random password based on length and character type selection(s):
 
-  for(var i = 0, n=userSelections.length; i<parseInt(confirmLength); i++) {
-    var finalPassword = userSelections[Math.floor(Math.random() * n)];
-  }
+  // var index = Math.floor(Math.random() * confirmLength.length);
+  // var userSelections = confirmLength[index];
+
+    for (var i = 0, n=userSelections.length; i<parseInt(confirmLength); i++) {
+         var finalPassword = userSelections[Math.floor(Math.random() * n)];
+    }
 
      console.log(finalPassword, 'final password')
      return finalPassword;
      
   }
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
